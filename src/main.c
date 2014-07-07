87fe43c9a2198746f9ae8118e386644a54875c22
nadavvin <email@nadavvin.com>
2014-07-08 00:04:28 +0300

    gtk to gdk

--- src/main.c
+++ src/main.c
@@ -13,15 +13,25 @@
 
 
 
-static GtkWidget*
+static GdkWindow*
 create_window (void)
 {
-	GtkWidget *window;
+	//GtkWidget *window;
+	GdkWindow *window;
+	GdkWindowAttr attr;
+	attr.window_type = GTK_WINDOW_TOPLEVEL;
+	attr.wclass = GDK_INPUT_OUTPUT;
+	attr.event_mask = GDK_KEY_PRESS_MASK | GDK_STRUCTURE_MASK | GDK_EXPOSURE_MASK;
+	attr.width = 400;
+    attr.height = 300;
+	attr.title = "gtk-foobar";
 
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (window), "gtk-foobar");
+	//window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	window = gdk_window_new (NULL, &attr, 0);
+	//gtk_window_set_title (GTK_WINDOW (window), "gtk-foobar");
 
-	gtk_window_set_keep_above (GTK_WINDOW (window), TRUE);
+	//gtk_window_set_keep_above (GTK_WINDOW (window), TRUE);
+	gdk_window_set_keep_above (window, TRUE);
 	//gtk_window_fullscreen (GTK_WINDOW (window));
 
 	GError *e = NULL;
@@ -41,14 +51,18 @@ create_window (void)
 	//cairo_paint (cr);
 	surface = gdk_cairo_surface_create_from_pixbuf (buf, 1, NULL);
 	r = gdk_cairo_region_create_from_surface (surface);
+	
 	if(!GDK_IS_WINDOW(window)) {
 		fprintf(stderr, "window is not a window\n");
 		return NULL;
 	}
 	gdk_window_shape_combine_region (window, r, 0, 0);
+	
 
 	/* Exit when the window is closed */
 	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+	
 	
 	return window;
 }
@@ -56,7 +70,7 @@ create_window (void)
 int
 main (int argc, char *argv[])
 {
- 	GtkWidget *window;
+ 	GdkWindow *window;
 
 
 
@@ -70,7 +84,8 @@ main (int argc, char *argv[])
 	gtk_init (&argc, &argv);
 
 	window = create_window ();
-	gtk_widget_show (window);
+	//gtk_widget_show (window);
+	gdk_window_show (window);
 
 	gtk_main ();
 
